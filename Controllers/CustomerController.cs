a563c8c8e405bf0f2acbfdd897be24949fe19b29
elnur88 <eliyevelnur88@gmail.com>
2020-07-29 17:57:01 +0400

    a

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using KidsWorld.Models.Class;
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +11,12 @@ namespace KidsWorld.Controllers
     public class CustomerController : Controller
     {
         // GET: Customer
+        Context c = new Context();
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var dgr = c.Customers.Where(x => x.Status == 0).ToList();
+            return View(dgr);
         }
     }
 }
