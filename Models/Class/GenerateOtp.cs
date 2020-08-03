1fab1cf7f7f771e72aba40dda38e50604826e3d4
Elnur Eliyev <eliyevelnur88@gmail.com>
2020-08-03 00:06:35 -0700

    otp

---
+++ Models/Class/GenerateOtp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KidsWorld.Models.Class
+{
+    public static class GenerateOtp
+    {
+
+        public static string GeneratePassword()
+        {
+            string OTPLength = "4";
+            string OTP = string.Empty;
+
+            string Chars = string.Empty;
+            Chars = "1,2,3,4,5,6,7,8,9,0";
+
+            char[] seplitChar = { ',' };
+            string[] arr = Chars.Split(seplitChar);
+            string NewOTP = "";
+            string temp = "";
+            Random rand = new Random();
+            for (int i = 0; i < Convert.ToInt32(OTPLength); i++)
+            {
+                temp = arr[rand.Next(0, arr.Length)];
+                NewOTP += temp;
+                OTP = NewOTP;
+            }
+            return OTP;
+        }
+    }
+}
